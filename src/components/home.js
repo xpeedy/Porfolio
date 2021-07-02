0adf2fb2dfa54204311daf20a7d9f5e711b995b9
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-07-02 05:26:58 -0400

    adds animation for name and adds dynamic data to projectCards

--- src/components/home.js
+++ src/components/home.js
@@ -11,11 +11,13 @@ const Home = () => {
     return(
         <div className="home">
             <div className="top">
-            <h1 className="name">Hello I'm Hairo</h1>
+                {/* <h1 className="hello">Hello</h1> */}
+                <h1 className="name">Hairo Garcia </h1>
+                <h3 className="role">Full Stack Web Developer</h3>
                 <div className="links">
                     <h3>Linkedin</h3>
                     <h3>Github</h3>
-                    <h3>something here</h3>
+                    <h3>Resume</h3>
                 </div>
             </div>
             <a href="#som-home"
--- src/components/projectsCards.js
+++ src/components/projectsCards.js
@@ -1,6 +1,4 @@
-import React, { useRef } from "react";
-import { useIntersection } from "react-use";
-import gsap from "gsap";
+import React, { useState } from "react";
 import projectsData from "../assets/projectsData";
 import "./projectsCards.css"
 
@@ -9,32 +7,32 @@ import "./projectsCards.css"
 
 const ProjectsCards = () => {
 
- 
+
 
     return(
         <div className="projects-container-home">
             <div className="project-1">
-                <img className="project-img-1" src="https://www.humanesociety.org/sites/default/files/styles/768x326/public/2018/08/puppy-410265.jpg?h=0c7c9985&itok=cSH64A3X"></img>
+                <img className="project-img-1" src={projectsData[0].picture}></img>
                 <div  className="project-info-1">
-                <h2 className="title-project-1">Title</h2>
-                <h3 className="description-project-1">descripton</h3>
-                <p className="paragraph-project-1">paragraph asdlkd lkasjdlk aksjs kadjd a jalsjd lkasdlka asd asjld lkj sd dkasdlksd jdksaj a jsdl aj </p>
+                <h2 className="title-project-1">{projectsData[0].name}</h2>
+                <h3 className="description-project-1">description</h3>
+                <p className="paragraph-project-1">{projectsData[0].description}</p>
                 </div>
             </div>
             <div className="project-2">
-                <img className="project-img-2" src="https://www.humanesociety.org/sites/default/files/styles/768x326/public/2018/08/puppy-410265.jpg?h=0c7c9985&itok=cSH64A3X"></img>
+                <img className="project-img-2" src={projectsData[1].name}></img>
                 <div className="project-info-2">
-                <h2 className="title-project-2">Title</h2>
+                <h2 className="title-project-2">{projectsData[1].name}</h2>
                 <h3 className="description-project-2">descripton</h3>
-                <p className="paragraph-project-2">paragraph asdlkd lkasjdlk aksjs kadjd a jalsjd lkasdlka asd asjld lkj sd dkasdlksd jdksaj a jsdl aj </p>
+                <p className="paragraph-project-2">{projectsData[1].description}</p>
                 </div>
             </div>
             <div className="project-3">
-                <img className="project-img-3" src="https://www.humanesociety.org/sites/default/files/styles/768x326/public/2018/08/puppy-410265.jpg?h=0c7c9985&itok=cSH64A3X"></img>
+                <img className="project-img-3" src={projectsData[2].name}></img>
                 <div className="project-info-3">
-                <h2 className="title-project-3">Title</h2>
+                <h2 className="title-project-3">{projectsData[2].name}</h2>
                 <h3 className="description-project-3">descripton</h3>
-                <p className="paragraph-project-3">paragraph asdlkd lkasjdlk aksjs kadjd a jalsjd lkasdlka asd asjld lkj sd dkasdlksd jdksaj a jsdl aj </p>
+                <p className="paragraph-project-3">{projectsData[2].description}</p>
                 </div>
             </div>
         </div> 
