b96a210801c7b0ecd027bde54d85b09589adb18a
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-07-02 03:47:55 -0400

    creates nav-bar.css file and addes underline to the nav links

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,55 +1,56 @@
 import { Link } from "react-router-dom";
 import Styled from "styled-components";
+import "./nav-bar.css";
 
 
 const NavBar = () => {
     
     return(
-        <Sdiv>
-            <div className="nav-container">
+        <div className="nav-container">
+            <div className="nav-wrapper">
             <img className="profile-pic" src="./mypic.jpg" alt="profile"/>
-            <Nav className="nav-bar">
+            <div className="nav-bar">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/projects">Projects</Link>
                 <Link className="nav-link" to="/contact">Contact Me</Link>
-            </Nav>
             </div>
-        </Sdiv>
+            </div>
+        </div>
     )
 }
 
 export default NavBar;
 
-const Sdiv = Styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    //border: solid white;
-    .nav-container {
-        display: flex;
-        width: 83%;
-        //border:solid red;
-        margin-top: 1%;
-    }
-    .profile-pic {
-        border-radius:50%;
-        width: 4%;
-    }
-`
-const Nav = Styled.nav`
-    display: flex;
-    justify-content: space-evenly;
-    width: 40%;
-    padding: 0%;
-    margin-left: 60%;
-    //border: solid white;
-    .nav-link {
-        display: flex;
-        text-decoration: none;
-        font-size: 170%;
-        color: white;
-        //border: solid red;
-        align-self: center;
-    }
-`;
+// const Sdiv = Styled.div`
+//     display: flex;
+//     justify-content: space-evenly;
+//     //border: solid white;
+//     .nav-wrapper {
+//         display: flex;
+//         width: 83%;
+//         //border:solid red;
+//         margin-top: 1%;
+//     }
+//     .profile-pic {
+//         border-radius:50%;
+//         width: 4%;
+//     }
+// `
+// const Nav = Styled.nav`
+//     display: flex;
+//     justify-content: space-evenly;
+//     width: 40%;
+//     padding: 0%;
+//     margin-left: 60%;
+//     //border: solid white;
+//     .nav-link {
+//         display: flex;
+//         text-decoration: none;
+//         font-size: 170%;
+//         color: white;
+//         //border: solid red;
+//         align-self: center;
+//     }
+// `;
 
