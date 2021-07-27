34716029726561ecc84f34d644e6a7f7c42f9874
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-07-27 16:17:23 -0400

    adds burger menu

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,29 +1,36 @@
 import React,{ useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { MenuOutlined } from "@ant-design/icons"
 import "./nav-bar.css";
 
 const navLinks = [
 {
     name: "Home",
-    link: "/" 
+    src: "/" 
 },
 {
     name: "Projects",
-    link: "/projects"
+    src: "/projects"
 },
 {
     name: "Contact",
-    link: "/contact"
+    src: "/contact"
 }
 ]
 
 const NavBar = () => {
-    const [links, setLinks] = useState(navLinks)
-    const [active, setActive] = useState(0)
+    // const [links, setLinks] = useState(navLinks)
+    // const [active, setActive] = useState(0)
+    const [ open, setOpen ] = useState(false);
 
-    const getClassName = (link) => {
-        return splitLocation[1] === link ? "nav-link active":"nav-link"
-      };
+    const display = () => {
+        setOpen(!open)
+    }
+
+
+    // const getClassName = (link) => {
+    //     return splitLocation[1] === link ? "nav-link active":"nav-link"
+    //   };
 
     // const markActive = id => {
     //     active === id ? setActive(null) : setActive(id)
@@ -38,21 +45,21 @@ const NavBar = () => {
      //Javascript split method to get the name of the path in array
      const splitLocation = pathname.split("/");
 
+     
+
     return(
         <div className="nav-container">
             <div className="nav-wrapper">
             <img className="profile-pic" src="./mypic.jpg" alt="profile"/>
-            {/* <div className="nav-bar">
-            {links.map((navItem, index) => (
-                <Link
-                id={index}
-                key={index} 
-                className={getClassName(navItem.link)}
-                to={navItem.link}
-                // onClick={(e) => markActive(e.target.id)}
-                >{navItem.name}</Link>
-            ))}
-            </div> */}
+
+            <MenuOutlined className="burger-menu-icon" onClick={display} style={{"color": "white"}}/>
+            { open ? <div className="burger-menu" onClick={display}>
+                <div className="burger-content">
+                {navLinks.map(link => (
+                        <Link className="burger-link" to={link.src}>{link.name}</Link>
+                ))}
+                </div>
+            </div> : ""}
             <div className="nav-bar">
                 <Link className={splitLocation[1] === "" ? "nav-link active" : "nav-link"} to="/">Home</Link>
                 <Link className={splitLocation[1] === "projects" ? "nav-link active" : "nav-link"} to="/projects">Projects</Link>
